86b26d78bd0d85e3b2511061b9170d7b25f93521
shuham-gupta-17 <39772611+shuham-gupta-17@users.noreply.github.com>
2018-07-30 21:55:06 +0530

    Add files via upload

---
+++ Cloud_talkback.ino
@@ -0,0 +1,98 @@
+#include <WiFi.h>
+
+#define LED_PIN   26
+
+const char* ssid     = "vivo Y21L";
+const char* password = "iamgupta";
+
+
+const char* host = "api.thingspeak.com";
+WiFiClient client;
+const int httpPort = 80;
+
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);
+    
+    Serial.begin(115200);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+}
+
+void loop()
+{
+    String url = "/talkbacks/26918/commands/execute.json?api_key=X3KRRUI2XENLYALF";
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+    
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    bool headerComplete = false;
+    int count;
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        if(headerComplete)
+        {
+            if(++count == 2)
+            {
+              int n1 = line.indexOf("command_string");
+              int n2 = line.indexOf(",", n1+17);
+              String cmd =  line.substring(n1+17, n2-1);
+              if(cmd != "")
+                Serial.println(cmd);
+              if(cmd == "ON")
+                digitalWrite(LED_PIN, HIGH);
+              if(cmd == "OFF")
+                digitalWrite(LED_PIN, LOW);
+                if(cmd=="blink")
+                {
+                   digitalWrite(LED_PIN,HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN,LOW);
+                      delay(1000);
+  
+                }
+                
+            }
+            
+        }
+        if(line == "\n")
+        {
+          headerComplete = true;
+          count = 0;
+        }
+
+    }
+   delay(1000);
+}
+
---
+++ LDR.ino
@@ -0,0 +1,16 @@
+
+#define pin 34
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensor=analogRead(34);
+  //int percent = map(sensor,0,4095,0,100); // a second resistance is used to convert the variation in resistance to variation in voltage
+  Serial.println(sensor);                  // wea are measuring voltage across LDR
+  delay(10);
+
+}
